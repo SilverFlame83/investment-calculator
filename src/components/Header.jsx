3448dc34bab7821a26d2795958e6ecd7b768b825
VelvetSyntax <imren_83@abv.bg>
2024-02-09 12:46:55 +0200

    adding header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+export function Header(){
+    return <header id="header">
+    <img src={logo} alt='Investment logo'/>
+    <h1>React Investment Calculator</h1>
+    </header>
+}
